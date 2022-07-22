e3baf486a5a7904750f6e44688cbed312e60aaa0
justinlim16 <justinlim.dn98@gmail.com>
2022-07-23 00:19:28 +0800

    Added buttons and read functionality

---
+++ Contact Tracing Viewer/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_read = New System.Windows.Forms.Button()
+        Me.btn_import = New System.Windows.Forms.Button()
+        Me.btn_reset = New System.Windows.Forms.Button()
+        Me.display = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(78, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(340, 46)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Contact Tracing Form"
+        '
+        'btn_read
+        '
+        Me.btn_read.Location = New System.Drawing.Point(101, 79)
+        Me.btn_read.Name = "btn_read"
+        Me.btn_read.Size = New System.Drawing.Size(94, 29)
+        Me.btn_read.TabIndex = 1
+        Me.btn_read.Text = "Read"
+        Me.btn_read.UseVisualStyleBackColor = True
+        '
+        'btn_import
+        '
+        Me.btn_import.Location = New System.Drawing.Point(201, 79)
+        Me.btn_import.Name = "btn_import"
+        Me.btn_import.Size = New System.Drawing.Size(94, 29)
+        Me.btn_import.TabIndex = 2
+        Me.btn_import.Text = "Import"
+        Me.btn_import.UseVisualStyleBackColor = True
+        '
+        'btn_reset
+        '
+        Me.btn_reset.Location = New System.Drawing.Point(301, 79)
+        Me.btn_reset.Name = "btn_reset"
+        Me.btn_reset.Size = New System.Drawing.Size(94, 29)
+        Me.btn_reset.TabIndex = 3
+        Me.btn_reset.Text = "Reset"
+        Me.btn_reset.UseVisualStyleBackColor = True
+        '
+        'display
+        '
+        Me.display.Location = New System.Drawing.Point(46, 125)
+        Me.display.Name = "display"
+        Me.display.Size = New System.Drawing.Size(416, 419)
+        Me.display.TabIndex = 4
+        Me.display.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(501, 566)
+        Me.Controls.Add(Me.display)
+        Me.Controls.Add(Me.btn_reset)
+        Me.Controls.Add(Me.btn_import)
+        Me.Controls.Add(Me.btn_read)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btn_read As Button
+    Friend WithEvents btn_import As Button
+    Friend WithEvents btn_reset As Button
+    Friend WithEvents display As RichTextBox
+End Class
---
+++ Contact Tracing Viewer/Form1.vb
@@ -0,0 +1,9 @@
+﻿Class Form1
+    Private Sub btn_read_Click(sender As Object, e As EventArgs) Handles btn_read.Click
+        Dim fileReader As System.IO.StreamReader
+        fileReader = My.Computer.FileSystem.OpenTextFileReader("C:\Users\justi\OneDrive\Desktop\Contact Tracing\Contact Tracing\bin\Debug\ContactTracingForm.txt")
+        Dim stringReader As String
+        stringReader = fileReader.ReadLine()
+        display.Text = fileReader.ReadToEnd
+    End Sub
+End Class
